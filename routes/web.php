68c21ae2fe8a7b49d52626b7201b5d56301b9473
Wiwikanggraini <71163874+Wiwikanggraini@users.noreply.github.com>
2020-11-30 06:19:39 -0800

    uts atw

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TagController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::resource('/category', CategoryController::class);
+Route::resource('/tag', TagController::class);
